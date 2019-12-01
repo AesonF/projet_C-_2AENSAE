aea11017b473ef5ae7095ddae8002a6c4e99f2af
AesonF <43008061+AesonF@users.noreply.github.com>
2019-12-01 17:39:29 +0100

    Add files via upload
    La multiplication matricielle fonctionne (enfin)

--- Black_Scholes/EqRes.hpp
+++ Black_Scholes/EqRes.hpp
@@ -13,16 +13,10 @@
 #include <stdio.h>
 #include <iostream>
 
-//classe "portefeuille" ?
+//classe des paramètres de modele
 class BSparams {
 public:
-    BSparams(int _n, int _m, float _tmax, float _sigma, float _mu){
-        n = _n;
-        m = _m;
-        tmax = _tmax;
-        sigma = _sigma;
-        mu = _mu;
-    }
+    BSparams(int _n, int _m, float _tmax, float _sigma, float _mu);
     int n;
     int m;
     float tmax;
@@ -30,58 +24,24 @@ public:
     float mu;
 };
 
-template <typename type>
 class Matrix {
 private:
     int n;
     int m;
-    type **p;
-    
-    void allocArray()
-    {
-        p = new type*[m];
-        for(int i=0; i<m; i++){
-            p[i] = new type[n];
-        }
-    }
+    float **p;
+    void allocArray();
 public:
-    Matrix(int a, int b){
-        n = a;
-        m = b;
-        allocArray();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                p[i][j] = 0;
-            }
-        }
-    }
-    
-    void load(int i, int j, type a){
-        p[i][j] = a;
-    }
-    
-    type iloc(int i, int j) { //get value (i,j)
-        return p[i][j];
-    }
-    
-    void show(){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                std::cout << p[i][j] << ',';
-            }
-            std::cout << std::endl;
-        }
-    }
-    
-    ~Matrix(){
-        for(int i = 0; i < m; i++){
-            delete [] p[i];
-        }
-        delete [] p;
-    }
+    Matrix(int a, int b); //constructeur d'une matrice de zeros
+    void load(int i, int j, float a); //insertion d'une valeur a en (i,j)
+    float iloc(int i, int j); //valeur a l'emplacement (i,j)
+    int lin(); //nombre de lignes
+    int col(); //nombre de colonnes
+    void show(); //affichage d'une matrice
+    Matrix operator *(Matrix &A);
+    ~Matrix();
 };
 
-void matCreate(BSparams &par, std::vector<float> &prices, Matrix<float> &Mat);
+void matCreate(BSparams &par, std::vector<float> &prices, Matrix &Mat);
 
 void BSSol(BSparams &par, std::vector<float> &prices);
 
--- Black_Scholes/main.cpp
+++ Black_Scholes/main.cpp
@@ -7,53 +7,27 @@
 //
 
 #include <iostream>
-#include <cmath>
-#include <random>
-#include <valarray>
+#include <vector>
 #include "EqRes.hpp"
 
-/* SINGLE SIMULATION :
- Cette première simulation du cours d'un produit dérivé dans le cadre du modèle de
- Black-Scholes se fait de manière itérative, dans une seule fonction. Arguments :
- - tmax  = temps de la simulation
- - n     = nombre de subdivisions temporelles
- - S0    = prix initial du sous-jacent
- - sigma = volatilité du sous-jacent
- - mu    = tendance du sous-jacent
-*/
-void singleSim(float tmax, int n, float S0, float sigma, float mu)
-{
-    float T[n]; T[0] = 0 ;  //Axe des abscisses (temps)
-    float S[n]; S[0] = S0; //Axe des ordonnées (prix du sous-jacent)
-    float S1 = 0;
-    float dt = tmax/n;
-    
-    std::default_random_engine generator;
-    std::normal_distribution<float> distribution(0.0,dt);
-    
-    float norm0 = 0;
-    float norm1 = 0;
-    for(int i=1; i<n+1 ; i++){
-        norm0 = norm1;
-        norm1 = distribution(generator);
-        S1 = S0*(mu*dt + sigma*(norm1+norm0) + 1);
-        S[i] = S1;
-        T[i] = tmax*i/n;
-        S0 = S1;
-    }
+void BSmatTest(){
+    BSparams par(5,5,1,0.01,0.02);
+    std::vector<float> prices{0.2,0.4,0.6,0.8,1.0};
+    BSSol(par, prices);
 }
 
-void valarrayExamples(){
-    Matrix<int> Mat(2,2);
-    Mat.load(0,0,1);
-    Mat.load(0,1,0);
-    Mat.load(1,0,0);
-    Mat.load(1,1,1);
-    Mat.show();
+void matTest(){
+    Matrix m1(2,2);
+    m1.load(0,0,1); m1.load(1,0,0); m1.load(0,1,0); m1.load(1,1,1);
+    m1.show();
+    Matrix m2(2,1);
+    m2.load(0,0,2); m2.load(1,0,1);
+    Matrix m3 = m1*m2;
+    m3.show();
 }
 
 int main(int argc, const char * argv[]) {
-    singleSim(10,1000,100,0.3,0.1);
-    valarrayExamples();
+    BSmatTest();
+    matTest();
     return 0;
 }
